de39a27ce4ae0877459be03376e871291be62f3d
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2013-06-06 15:03:03 +0000

    ARM: be strict about FP exceptions in kernel mode
    The support code in vfp_support_entry does not care whether the
    exception that caused it to be invoked occurred in kernel mode or
    in user mode. However, neither condition that could trigger this
    exception (lazy restore and VFP bounce to support code) is
    currently allowable in kernel mode.
    
    In the former case, we can just handle it as an undefined instruction.
    In the latter case, we should flag it as a bug, as it implies that
    the FP unit has been enabled and an attempt has been made to
    execute FP instructions that are dependent on the support code, and
    this is not supported in kernel mode.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Acked-by: Nicolas Pitre <nico@linaro.org>

--- arch/arm/vfp/vfpmodule.c
+++ arch/arm/vfp/vfpmodule.c
@@ -28,6 +28,7 @@
 #include <asm/system_info.h>
 #include <asm/thread_notify.h>
 #include <asm/vfp.h>
+#include <asm/bug.h>
 
 #include "vfpinstr.h"
 #include "vfp.h"
@@ -676,6 +677,16 @@ void kernel_neon_end(void)
 EXPORT_SYMBOL(kernel_neon_end);
 
 #endif /* CONFIG_KERNEL_MODE_NEON */
+
+void vfp_kmode_exception(void)
+{
+	/*
+	 * Taking an FP exception in kernel mode is always a bug, because
+	 * none of the FP instructions currently supported in kernel mode
+	 * (i.e., NEON) should ever be bounced back to the support code.
+	 */
+	BUG_ON(fmrx(FPEXC) & FPEXC_EN);
+}
 
 /*
  * VFP hardware can lose all context when a CPU goes offline.
